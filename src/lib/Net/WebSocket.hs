02113dd2e63d6a9905bbc8d70b6add32b94deec7
ckkashyap <ckkashyap@gmail.com>
2012-08-19 12:55:11 +0530

    working version

--- src/lib/Net/WebSocket.hs
+++ src/lib/Net/WebSocket.hs
@@ -7,18 +7,17 @@ import Net.WebSocket.Request
 
 websocket :: SI.Handle -> IO ()
 websocket handle = do
-	requestHeader <- readRequest handle
-	
-	putStrLn $ show requestHeader
-        putStrLn $ show web_socket_guid     
-        putStrLn $ show opcode_continuation 
-        putStrLn $ show opcode_text         
-        putStrLn $ show opcode_binary       
-        putStrLn $ show opcode_close        
-        putStrLn $ show opcode_ping         
-        putStrLn $ show opcode_pong         
-
-	return ()
+	request@(Request fl _ ) <- readRequest handle
+	if (take 3 fl)  == "BAD" then putStrLn (show request) >> SI.hClose handle >> return () else do
+		putStrLn $ show request
+		putStrLn $ show web_socket_guid     
+		putStrLn $ show opcode_continuation 
+		putStrLn $ show opcode_text         
+		putStrLn $ show opcode_binary       
+		putStrLn $ show opcode_close        
+		putStrLn $ show opcode_ping         
+		putStrLn $ show opcode_pong         
+		return ()
 
 
 
--- src/lib/Net/WebSocket/Request.hs
+++ src/lib/Net/WebSocket/Request.hs
@@ -1,7 +1,11 @@
 module Net.WebSocket.Request
-       (readRequest) where
+       (readRequest,Request (Request) ) where
 
 import qualified System.IO as SI
+import qualified Control.Monad as CM
+import qualified Control.Monad.Trans as CMT
+import qualified Control.Monad.Trans.Error as CMTE
+
 
 data Request = Request {firstLine :: String, header :: [(String,String)]}
 
@@ -11,35 +15,60 @@ instance Show Request where
 		       hdrString (('\r':[],v):ls) = hdrString ls
 		       hdrString ((k,v):ls) = "<" ++ k ++ " -> " ++ v ++ ">\n" ++ hdrString ls
 
+
 readRequest :: SI.Handle -> IO Request
 readRequest handle = do
-	fl <- SI.hGetLine handle
-	hdr <- readHeader handle
-	return Request {header=hdr, firstLine=fl}
+	request' <- CMTE.runErrorT (readRequest' handle)
+	let (request , action) = case request' of 
+	             Right r  -> (r,putStrLn "Parsed the request successfully")
+		     Left err -> (Request ("BAD REQUEST : " ++ err) [], putStrLn err)
+	action
+	return request
+	
 
 
-readHeader :: SI.Handle -> IO [(String, String)]
+
+
+--do
+--	r <- CMTE.ErrorT $ readRequest' handle
+--	let action = case r of
+--			Left error -> putStrLn error
+--			Right _ -> putStrLn "Parsed the request successfully"
+--	CMT.lift action
+--	return (Request "" [])
+
+readRequest' :: SI.Handle -> CMTE.ErrorT String IO Request
+readRequest' handle = do
+	fl <- CMTE.ErrorT (readFirstRequestLine handle)
+	hdr <- CMTE.ErrorT (readHeader handle)
+	let request = Request {header=hdr, firstLine=fl}
+	return  request
+
+
+readFirstRequestLine :: SI.Handle -> IO (Either String String)
+readFirstRequestLine handle = do
+                                l <- SI.hGetLine handle
+				return $ validateFirstLine l
+
+readHeader :: SI.Handle -> IO (Either String [(String, String)])
 readHeader h = do
   rawLine <- SI.hGetLine h
   let hdr = takeWhile (/= ':') rawLine
   let val = takeWhile (/= '\r') $ drop (length hdr + 2) rawLine
-  rest <- if hdr /= "\r" then readHeader h else return []
-  return ((hdr,val):rest)
+  (Right rest) <- if hdr /= "\r" then readHeader h else return (Right [])
+  let headers = (hdr,val):rest
+  return (validate headers)
  
-getHeaderValue :: String -> [String] -> String
-getHeaderValue _ [] = []               
-getHeaderValue h (s:ss) = if rightHeader then extractKey else getHeaderValue h ss where
-                             headerField = h ++ ": "
-                             lengthOfHeaderField = length headerField
-                             extractKey = drop lengthOfHeaderField s
-                             rightHeader = (take lengthOfHeaderField s) == headerField
 
-validateFirstLine :: [String] -> Maybe [String]
-validateFirstLine (l:ls) = if take 3 l == "GET" then return ls else Nothing
+validateFirstLine :: String -> Either String String
+validateFirstLine l = if take 3 l == "GET" then Right l else  Left $ "The first line of the request if faulty: " ++ l
 
-validateRequest :: [String] -> Maybe [String]
-validateRequest header = do
-  validateFirstLine header
-  return header
-  
-                  
+--getHeaderValue :: Request -> String -> Either String String
+--getHeaderValue _ [] = []               
+--getHeaderValue h (s:ss) = if rightHeader then extractKey else getHeaderValue h ss where
+--                             headerField = h ++ ": "
+--                             lengthOfHeaderField = length headerField
+--                             extractKey = drop lengthOfHeaderField s
+--                             rightHeader = (take lengthOfHeaderField s) == headerField
+
+validate x = Right x  
